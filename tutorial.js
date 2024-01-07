7c6ee1ee022872aea676280be75af106ca728195
crystalyn <00crystalynthompson@gmail.com>
2024-01-06 22:31:40 -0700

    rock paper scissors is working!!

--- tutorial.js
+++ tutorial.js
@@ -1,14 +1,16 @@
 const computerchoicedisplay =document.getElementById('computerchoice')
 const userchoicedisplay =document.getElementById('userchoice')
-const result = document.getElementById('result')
+const resultdisplay = document.getElementById('result')
 const possible = document.querySelectorAll('button')
 let userchoice
 let computerchoice
+let result
 
 possible.forEach(possible => possible.addEventListener('click', (e) =>{
 userchoice = e.target.id
 userchoicedisplay.innerHTML = userchoice
 gencompchoice()
+getresult()
 }))
 
 function gencompchoice(){
@@ -18,4 +20,14 @@ function gencompchoice(){
    if(randomnumber === 2){ computerchoice='scissors'}
    computerchoicedisplay.innerHTML = computerchoice
 }
-function getresult(){}
+function getresult(){
+    if (computerchoice === userchoice){ result ='a draw!'}
+    if (computerchoice === 'rock' && userchoice === 'paper'){result= 'you win!'}
+    if (computerchoice === 'rock' && userchoice === 'scissors'){result='failed'}
+    if (computerchoice === 'paper' && userchoice === 'rock'){result= 'failed'}
+    if (computerchoice === 'paper' && userchoice === 'scissors'){result='you win!'}
+    if (computerchoice === 'scissors' && userchoice === 'rock'){result='you win!'}
+    if (computerchoice === 'scissors' && userchoice === 'paper'){result='failed'}
+    resultdisplay.innerHTML= result
+}
+resultdisplay.innerHTML= result
