998967eb5bcf2ed38a5e5d5e63e919626f74780f
crystalyn <00crystalynthompson@gmail.com>
2024-01-08 19:21:47 -0700

    starting new game

---
+++ memorygame.js
@@ -0,0 +1,7 @@
+const card array=[
+    {name:'elephant', img:'pics/lilelephant.png'}
+    {name:'giraffe', img:'pics.lilgiraffe.png'}
+    {name:'flamingo', img:'pics/lilflamingo.png'}
+    {name:'bunny', img:'pics/lilbunny.png'}
+    {name:'lion', img:'pics/lillion.png'}
+    {name:'snake', img:'pics/lilsnake.png'}]
