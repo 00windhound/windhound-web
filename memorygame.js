81d8b4bccabc6b7e5a798066fdfc3808b45db12c
crystalyn <00crystalynthompson@gmail.com>
2024-01-08 23:28:21 -0700

    finished tutorial, mostly working but i want to change how you keep score

--- memorygame.js
+++ memorygame.js
@@ -15,9 +15,11 @@ const cardarray=[
     cardarray.sort(() => 0.5 - Math.random())
 
    const grid = document.querySelector('#grid')
+   const resultdisplay = document.querySelector('result')
     let cardschosen =[]
     let cardschosenids =[]
     const cardswon = []
+
 
    function makeboard (){
     for(let i=0; i< 12; i++){
@@ -25,36 +27,45 @@ const cardarray=[
       card.setAttribute('src','pics/lilcard.png' )
       card.setAttribute('data-id', i)
       card.addEventListener('click', flipcard)
-    grid.appendChild(card)
-    }
+    grid.appendChild(card) }
    }
    makeboard()
+
+
  function flipcard(){
    const cardid = this.getAttribute('data-id')
    cardschosen.push(cardarray[cardid].name)
    cardschosenids.push(cardid)
    this.setAttribute('src', cardarray[cardid].img)
    if(cardschosen.length === 2){
-    setTimeout(checkmatch, 500)
+    setTimeout(checkmatch, 500) }
    }
-   }
+
 
    function checkmatch(){
   const cards =  document.querySelectorAll('img')
     const optiononeid = cardschosenids[0]
     const optiontwoid = cardschosenids[1]
 
-if()
+if(optiononeid == optiontwoid){alert('dont click the same card')
+cards[optiononeid].setAttribute('src', 'pics/lilcard.png')
+cards[optiontwoid].setAttribute('src', 'pics/lilcard.png')}
 
-   if (cardschosen[0] == cardschosen[1]){console.log('you found a match')
-    cards[cardschosenids[0]].setAttribute('src', 'pics/lilswoosh.png')
-    cards[cardschosenids[1]].setAttribute('src', 'pics/lilswoosh.png')
-    cards[cardschosenids[0]].removeEventListener('click', flipcard)
-    cards[cardschosenids[1]].removeEventListener('click', flipcard)
+   if (cardschosen[0] == cardschosen[1]){
+    cards[optiononeid].setAttribute('src', 'pics/lilswoosh.png')
+    cards[optiontwoid].setAttribute('src', 'pics/lilswoosh.png')
+    cards[optiononeid].removeEventListener('click', flipcard)
+    cards[optiontwoid].removeEventListener('click', flipcard)
     cardswon.push(cardschosen)
-    
+}
+else{
+    cards[optiononeid].setAttribute('src', 'pics/lilcard.png')
+    cards[optiontwoid].setAttribute('src', 'pics/lilcard.png')
 }
    cardschosen = []
    cardschosenids=[]
+   resultdisplay.innerHTML = cardswon.length
+
+   if(cardswon.length == cardarray.length/2){resultdisplay.innerHTML= 'you win'}
    } /* i want to make somewhere that i can display this */
 
