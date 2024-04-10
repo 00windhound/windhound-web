9776b7f15417464a953664784bd178fef9c3189a
crystalyn <00crystalynthompson@gmail.com>
2024-04-09 19:43:43 -0600

    works

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -3,6 +3,18 @@ const userchoicedisplay =document.getElementById('userchoice')
 const resultdisplay = document.getElementById('result')
 const possible = document.querySelectorAll('button')
 const computerpic = document.getElementById('computerpic')
+const rock1 = document.createElement("img");
+rock1.src = "pics/rock.png";
+const paper1 = document.createElement("img")
+paper1.src = "pics/paper.png";
+const scissors1 = document.createElement("img")
+scissors1.src = "pics/scissors.png";
+const greyrock1 = document.createElement("img");
+greyrock1.src = "pics/greyrock.png";
+const greypaper1 = document.createElement("img");
+greypaper1.src = "pics/greypaper.png";
+const greyscissors1 = document.createElement("img");
+greyscissors1.src = "pics/greyscissors.png";
 const rock = document.createElement("img");
 rock.src = "pics/rock.png";
 const paper = document.createElement("img")
@@ -15,10 +27,11 @@ const greypaper = document.createElement("img");
 greypaper.src = "pics/greypaper.png";
 const greyscissors = document.createElement("img");
 greyscissors.src = "pics/greyscissors.png";
+
 let userchoice
 let computerchoice
 let result
-/* freehanding */
+
 const wincountdisplay =document.getElementById('wincount')
 const drawcountdisplay =document.getElementById('drawcount')
 const losecountdisplay =document.getElementById('losecount')
@@ -28,19 +41,50 @@ let wincount = 0
 let drawcount = 0
 let losecount= 0
 
-possible.forEach(possible => possible.addEventListener('click', (e) =>{
-userchoice = e.target.id
-userchoicedisplay.innerHTML = userchoice
+function pickme(pick){
+//possible.forEach(possible => possible.addEventListener('click', (e) =>{
+userchoice = pick
+//userchoicedisplay.innerHTML = userchoice
+if(userchoice === 'rock'){
+    //document.getElementById('comppics').()
+    document.getElementById('rockbbt').replaceChildren()
+    document.getElementById('rockbbt').appendChild(rock1)
+    document.getElementById('paperbbt').replaceChildren()
+    document.getElementById('paperbbt').appendChild(greypaper1)
+    document.getElementById("scissorsbbt").replaceChildren()
+    document.getElementById("scissorsbbt").appendChild(greyscissors1)
+}
+else if(userchoice === 'paper'){
+    //document.getElementById('comppics').()
+    document.getElementById('rockbbt').replaceChildren()
+    document.getElementById('rockbbt').appendChild(greyrock1)
+    document.getElementById('paperbbt').replaceChildren()
+    document.getElementById('paperbbt').appendChild(paper1)
+    document.getElementById("scissorsbbt").replaceChildren()
+    document.getElementById("scissorsbbt").appendChild(greyscissors1)
+
+}
+else if(userchoice === 'scissors'){
+    //document.getElementById('comppics').replaceChildren()
+    document.getElementById('rockbbt').replaceChildren()
+    document.getElementById('rockbbt').appendChild(greyrock1)
+    document.getElementById('paperbbt').replaceChildren()
+    document.getElementById('paperbbt').appendChild(greypaper1)
+    document.getElementById("scissorsbbt").replaceChildren()
+    document.getElementById("scissorsbbt").appendChild(scissors1)
+}
+
 gencompchoice()
 getresult()
 counter()
-}))
+//}))
+}
 
 function gencompchoice(){
     const randomnumber =Math.floor(Math.random() * possible.length)
    if(randomnumber === 0){ computerchoice ='rock'}
-   if(randomnumber === 1){ computerchoice='paper'}
-   if(randomnumber === 2){ computerchoice='scissors'}
+   else if(randomnumber === 1){ computerchoice='paper'}
+   else if(randomnumber === 2){ computerchoice='scissors'}
    computerchoicedisplay.innerHTML = computerchoice
     if(computerchoice === 'rock'){
         //document.getElementById('comppics').()
@@ -70,7 +114,7 @@ function gencompchoice(){
         document.getElementById("compscissors").replaceChildren()
         document.getElementById("compscissors").appendChild(scissors)
     }
-}
+}//model the user buttons to look and act like the computer buttons so it feels more fair
 function getresult(){
     if (computerchoice === userchoice){ result ='a draw!'}
     if (computerchoice === 'rock' && userchoice === 'paper'){result= 'you win!'}
